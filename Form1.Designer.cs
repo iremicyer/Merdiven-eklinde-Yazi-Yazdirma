e4b5fa954874f2c431ea6f0cd3844fd06de50dc2
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-05 16:41:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Merdiven_Yazdırma_Console
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonYazdır = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LightSalmon;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 334);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonYazdır
+            // 
+            this.buttonYazdır.BackColor = System.Drawing.Color.Bisque;
+            this.buttonYazdır.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonYazdır.Location = new System.Drawing.Point(438, 132);
+            this.buttonYazdır.Name = "buttonYazdır";
+            this.buttonYazdır.Size = new System.Drawing.Size(103, 91);
+            this.buttonYazdır.TabIndex = 1;
+            this.buttonYazdır.Text = "YAZDIR";
+            this.buttonYazdır.UseVisualStyleBackColor = false;
+            this.buttonYazdır.Click += new System.EventHandler(this.buttonYazdır_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Bisque;
+            this.textBox1.Location = new System.Drawing.Point(438, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSalmon;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(349, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KELİME =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(665, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonYazdır);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonYazdır;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Merdiven_Yazdırma_Console    // HEM AŞAĞIYA DOĞRU HEM DE SAĞA DOĞRU BİR ARTMA OLACAĞI İÇİN İKİ BOYUTLU DİZİ KULLANILDI.
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonYazdır_Click(object sender, EventArgs e)
+        {
+
+            string[,] kelime = new string[textBox1.Text.Length, textBox1.Text.Length]; //iki boyutlu dizinin, kelimenin boyutu kadar olmasını sağlandı.
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    kelime[i, j] = textBox1.Text[j].ToString();
+
+                }
+            }
+
+            listBox1.Text = ""; // listbox' ı temizliyoruz.
+
+
+
+
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                string s = "";
+                for (int j = 0; j <= i; j++)
+                {
+                    s += kelime[i, j];
+
+                }
+                listBox1.Items.Insert(i, s);
+            }
+
+        }
+    }
+}
